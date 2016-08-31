2f62cdf42c3cd9355a1339788cc6d11540f4a9ef
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-08-30 21:46:38 -0600

    finished project

---
+++ js/dataService.js
@@ -0,0 +1,48 @@
+angular.module('quoteBook').service('dataService', function(){
+  var quotes = [];
+
+  this.getData = function() {
+    var local = getLocalData();
+    if (!local) {
+      quotes = [
+        { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
+        { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
+        { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
+        { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
+        { text: 'The most difficult thing is the decision to act, the rest is merely tenacity.', author: 'Amelia Earhart'},
+        { text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
+        { text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
+      ];
+      return quotes;
+    }
+    return local;
+  };
+
+  this.addData = function(newQuote) {
+    if (newQuote.text && newQuote.author) {
+      quotes.push(newQuote);
+      setLocalData(quotes);
+      return true;
+    }
+      return false;
+    };
+
+  this.removeData = function(textToRemove) {
+    for (var i = 0; i < quotes.length; i++) {
+      if (quotes[i].text.toLowerCase() === textToRemove.toLowerCase()) {
+        quotes.splice(i--, 1);
+        setLocalData(quotes);
+      }
+    }
+  };
+
+  function setLocalData(quotes) {
+    localStorage.setItem('quotes', JSON.stringify(quotes));
+  }
+
+  function getLocalData() {
+    quotes = JSON.parse(localStorage.getItem('quotes'));
+    return quotes;
+  }
+
+});
